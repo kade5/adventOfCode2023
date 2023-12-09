c39221110b79bf61faf1e75c46d660e3253ea985
Zeke <soto.zeke5@gmail.com>
2023-12-08 19:07:50 -0600

    day2 part2 solution

--- bin/day2.ml
+++ bin/day2.ml
@@ -41,12 +41,65 @@ let rec is_set_valid set =
   | _ -> false
 
 (* let () = Printf.printf "%i\n" (get_game_id "Game 4") *)
+let max_powerset pset1 pset2 = List.map2 max pset1 pset2
 
 let rec is_game_valid subset_list =
   match subset_list with
   | [] -> true
   | hd :: rest when is_set_valid hd -> is_game_valid rest
   | _ -> false
+
+let get_powerset set =
+  let rec aux power_set set =
+    match set with
+    | [] -> power_set
+    | hd :: rest when List.nth hd 1 = "red" ->
+        aux
+          [
+            int_of_string (List.nth hd 0);
+            List.nth power_set 1;
+            List.nth power_set 2;
+          ]
+          rest
+    | hd :: rest when List.nth hd 1 = "green" ->
+        aux
+          [
+            List.nth power_set 0;
+            int_of_string (List.nth hd 0);
+            List.nth power_set 2;
+          ]
+          rest
+    | hd :: rest when List.nth hd 1 = "blue" ->
+        aux
+          [
+            List.nth power_set 0;
+            List.nth power_set 1;
+            int_of_string (List.nth hd 0);
+          ]
+          rest
+    | _ :: rest -> aux power_set rest
+  in
+  aux [ 0; 0; 0 ] set
+
+let get_power subset_list =
+  let rec aux power_set subset_list =
+    match subset_list with
+    | [] -> power_set
+    | hd :: rest -> aux (max_powerset power_set (get_powerset hd)) rest
+  in
+  List.fold_left ( * ) 1 (aux [ 0; 0; 0 ] subset_list)
+
+let sum_power input =
+  let rec aux sum_power input =
+    match input with
+    | [] -> sum_power
+    | hd :: rest ->
+        let game = split_and_trim ':' hd in
+        let subset = split_and_trim ';' (List.nth game 0) in
+        let subset_list = split_subsets subset in
+        aux (sum_power + get_power subset_list) rest
+  in
+  aux 0 input
 
 let sum_ids input =
   let rec aux sum_games input =
@@ -63,17 +116,9 @@ let sum_ids input =
   aux 0 input
 
 let input = Advent.read_lines "day2.txt"
-(* let str = *)
-(*   "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red" *)
-
-(* let game = split_and_trim ':' str *)
-(* let subsets = split_and_trim ';' (List.nth game 0) *)
 
 let part1_total = sum_ids input
+let part2_total = sum_power input
 let () = Printf.printf "The answer to part 1 is %i\n" part1_total
+let () = Printf.printf "The answer to part 2 is %i\n" part2_total
 
-(* let game_id = get_game_id (List.nth game 0) *)
-(* let subset_list = split_subsets subsets *)
-(* let result = is_game_valid subset_list *)
-(* let () = Printf.printf "Game ID: %i\n" (get_game_id (List.nth game 1)) *)
-(* let () = Printf.printf "Valid? %s\n" (string_of_bool result) *)
